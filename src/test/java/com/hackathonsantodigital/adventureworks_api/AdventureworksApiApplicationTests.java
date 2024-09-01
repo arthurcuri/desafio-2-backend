39104d06e582f2abe1eb43fe774a0d8528b9f14f
Arthur Kramberger <arthur1curi@gmail.com>
2024-09-01 19:08:14 -0300

    Projeto iniciado

---
+++ src/test/java/com/hackathonsantodigital/adventureworks_api/AdventureworksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonsantodigital.adventureworks_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureworksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
